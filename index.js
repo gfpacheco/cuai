742be5f4df5ac404808a54f3603661427ff22a2f
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2017-02-07 01:27:41 -0200

    Create server

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // TODO:
-// [ ] create http server
+// [x] create http server
 // [ ] listen requests
 // [ ] implement dumb ai (plays if can)
 // [ ] implement better ai (probably will never be finished)
+
+const restify = require('restify');
+
+const port = process.env.PORT || 3000;
+const server = restify.createServer();
+
+server.listen(port, function() {
+  console.log(`CuAI listening on port ${port}!`);
+});
